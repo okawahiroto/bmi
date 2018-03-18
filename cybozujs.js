69ecf277514f09e42a3a9de8a9328a33a1605ebd
okawahiroto <okawa.hiroto@gmail.com>
2018-03-18 14:12:09 +0900

    コメント記入

--- cybozujs.js
+++ cybozujs.js
@@ -1,50 +1,23 @@
-console.log('a');
-
-// var result = window.confirm('確認ダイアログが表示されます');
-
-// if (result) {
-//   console.log('はい　が押されました');
-// } else {
-//   console.log('キャンセル　が押されました');
-// }
-
-var score = 90;
-
-if (score > 80) {
-  console.log('やったね、合格です');
-} else {
-  console.log('残念、不合格です');  
-}
-
-// BMI 体重(kg) / 身長(m)の二乗
-
-// BMI < 18.5 yase
-// 18.5 < BMI < 25 hyojun
-// 25 < BMI < 30 太り気味
-// 30 < BMI himan
-
-// var height = prompt('あなたの身長(m)を入力してください', '');
-// var weight = prompt('あなたの体重(kg)を入力してください', '');
-
-
-
-// var text = element.textContent;
-// element.textContent = "this is some sample text";
-// console.log(text);
-
+// htmlの読み込みが終わってから実行
 window.onload = function() {
 
+  // htmlの要素取得
   var height = document.getElementById("height").textContent;
   var weight = document.getElementById("weight").textContent;
   var bmi = document.getElementById("bmi").textContent;
   var result = document.getElementById("result").textContent;  
 
+  // ダイアログで身長・体重入力
   var h = prompt('あなたの身長(m)を入力してください', '');
   var w = prompt('あなたの体重(kg)を入力してください', '');
   
+  // BMIの計算
   var b = w / (h * h);
+
+  // BMIの値を小数点第二位で四捨五入
   var x = Math.round(b * 10) / 10;
 
+  // 結果の変数
   var r = "";
   
   if (b < 18.5) {
@@ -57,9 +30,9 @@ window.onload = function() {
     r = '肥満体型'; 
   }
 
+  // htmlの各要素へ入力
   document.getElementById("height").textContent = h;
   document.getElementById("weight").textContent = w;
   document.getElementById("bmi").textContent = x;
   document.getElementById("result").textContent = r;
-
 }
